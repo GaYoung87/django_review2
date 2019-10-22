f7fa0b0a8defe8376b730d7579a1ac5ecc30d55c
GaYoung87 <gyyoon4u@naver.com>
2019-10-22 11:38:32 +0900

    Account
     - password바꾸기 -> 비밀번호 바뀌면 세션hash값이 바뀌어 로그아웃 ->
     로그아웃 되지않고 password바꾸기
     - account update
     - accounts에있는 여러 html들을 form.html로 묶어 표시

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from . import views
+
 app_name = 'accounts'
 
 # urlpatterns가 있어야 error가 안남 -> list 우선적으로 만들자!
 urlpatterns = [
-    # 회원가입 url
+    # 회원가입 url 
     path('signup/', views.signup, name='signup'),
     # 로그인 url
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     # 회원탈퇴
     path('delete/', views.delete, name='delete'),
+    path('update/', views.update, name='update'),
+        # 로그인이 되어있기때문에, user의 정보를 id값으로 판별할 필요가 없음 -> pk값 필요없음
+    path('password/', views.password, name='password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 # UserCreationForm을 통해 user에 대한 정보 가지고옴
-from django.contrib.auth import login as auth_login, logout as auth_logout
+# UserChangeForm : 유저 정보를 수정하기 위한 form
+from django.contrib.auth import login as auth_login, logout as auth_logout, update_session_auth_hash
 # Authentication에 대한 행위를 할 수 있음 import login으로 로그인할 수 있음.
 from django.views.decorators.http import require_POST
+from .forms import CustomUserChangeForm
+from django.contrib.auth.decorators import login_required
+
 
 # 회원가입 form이 제공되어야함
 # get: 처음에 signup 눌렀을 때 
@@ -23,9 +27,10 @@ def signup(request):
         form = UserCreationForm
     context = {'form': form}
     # 회원가입 페이지 주세요
-    return render(request, 'accounts/signup.html', context)
+    return render(request, 'accounts/form.html', context)
 
 
+# 세션의 정보를 db로 저장
 def login(request):  # from django.contrib.auth import login과 이름이 같다.
     # POST: 로그인 시켜줌
     if request.method == 'POST':
@@ -33,13 +38,13 @@ def login(request):  # from django.contrib.auth import login과 이름이 같다
         form = AuthenticationForm(request, request.POST)  # AuthenticationForm에만 첫번째 인자로 request가 들어간다.
         if form.is_valid():
             auth_login(request, form.get_user())  # 사용자의 정보를 주는 함수: form.get_user()
-            next_page = request.POST.get('next')
+            next_page = request.POST.get('next')  
             return redirect(next_page or 'articles:index')
 
     else:  # GET: 로그인 페이지 보여줌
         form = AuthenticationForm()
     context = {'form': form}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/form.html', context)
 
 
 # 로그아웃: sessionid 지우기
@@ -55,3 +60,36 @@ def delete(request):
     if request.user.is_authenticated:
         request.user.delete()
     return redirect('articles:index')
+
+
+@login_required
+# 바뀐 정보만 전송
+def update(request):
+    # 실제 수정해주세요라는 요청이 들어올 때
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+
+    # 수정할 수 있는 페이지를 주세요라는 요청이 들어올 때
+    else:
+        form = CustomUserChangeForm(instance=request.user)  # 기존의 정보 가지고서 update시켜야함.
+    context = {'form': form}
+    return render(request, 'accounts/form.html', context)
+
+
+@login_required
+# password는 회원정보 수정에서 할 수 없음 -> 장고가 이렇게 설정해놓음. -> 새로 만들어야함.
+# password라는 url을 만들고, view함수도 만들어라 -> 장고가 제공하는 기본
+def password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('accounts:update')
+    else:
+        form = PasswordChangeForm(request.user)  # 사용 방법 다름
+    context = {'form': form}
+    return render(request, 'accounts/form.html', context)
